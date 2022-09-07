f4740ad3b305ba887e4538e641a7644d65152c5c
jiho1996 <jiho.urssu@gmail.com>
2022-09-07 18:50:31 +0900

    feat : 이미지 업로드 기능 추가

---
+++ upload.js
@@ -0,0 +1,85 @@
+class upload{
+    constructor(){
+        
+        this.render()
+        
+    }
+
+
+
+
+    render (){
+        const imageUrl = ''
+        document.querySelector("#pic-submit-button").addEventListener("click", this.setImage)
+        console.log(imageUrl)
+        document.querySelector("#submit-button").addEventListener("click", this.onSubmit)
+        this.getImage();
+    }
+
+    getImage(){
+        const inputImage = document.getElementById("upload-img-placeholder")
+            inputImage.addEventListener("change", (e) => {
+                // console.log(document.getElementById("upload-name").value)
+                // console.log(document.getElementById("upload-price").value)
+                // console.log(document.getElementById("product-description").value)
+                readImage(e.target)
+        function readImage(input){
+
+            if (input.files && input.files[0]){
+                const reader = new FileReader()
+
+                reader.onload = e => {
+                    const previewImage = document.querySelector("#preview-image");
+                    previewImage.src = e.target.result
+                }
+                reader.readAsDataURL(input.files[0])
+            }
+        }
+    
+            })
+    }
+    
+    setImage(){
+        
+            let strURL = 'http://localhost:8080/image'
+            
+            let form_data = new FormData()
+
+            form_data.append("image", document.querySelector("#upload-img-placeholder").files[0])
+        
+
+            //form_data.append("")
+
+            // for (var pair of form_data.entries()) {
+            //     console.log(pair[0]+ ', ' + pair[1]); 
+            // }
+            const init = {
+                method: 'POST',
+                // headers: {
+                //     // 'Content-Type': 'application/x-www-form-urlencoded',
+                //     // 'credentials': 'same-origin'
+                // },
+                body: form_data
+            }
+        
+            fetch(strURL, init)
+                .then(async (res) => {
+                    if (res.status == 200) {
+                        console.log("success")
+                        await res.json()
+                        
+                    } else {
+                        console.error(`HTTP ERROR! status${res.status}`)
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        
+
+    }
+    }
+
+
+
+new upload();
