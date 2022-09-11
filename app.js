1e29d1f552bdcd53747a5e83fb68bbfe59f8a0fe
jiho1996 <jiho.urssu@gmail.com>
2022-09-11 16:56:19 +0900

    feat : soldout시 blur처리 로직

--- app.js
+++ app.js
@@ -35,9 +35,18 @@ class app {
         const getProducts = await axios.get(url);
         //console.log(getProducts.data.products)
         const productLists = getProducts.data.products.map((item) => {
+            // function getSoldOut(){
+            //     let soldout = document.createElement("div")
+            //     soldout.className = "product-blur"
+            //     document.querySelector(`#product-card-${item.name}`).appendChild("soldout")
+            // }
+            // item.soldout === 1 ? getSoldOut() : ''
+            //${item.soldout === 1 ? <div class ="product-blur"> : ''}
+
             // 서버에서 static파일 지정 덕에 소스주소를 아래와 같이해도 이미지 렌더링 가능.
             return `
-        <div class = "product-card">
+        <div class = "product-card" id ="product-card-${item.name}">
+        ${item.soldout === 1 ? `<div class ="product-blur">` : `<div>`}
         <img class = "product-img" src="http://localhost:8080/${item.imageUrl}"/>
             <div class = "product-content">
                 <span class="product-name">${item.name}</span>
@@ -51,18 +60,25 @@ class app {
                             </div>
                                 </div>
                                 </div>
+                                
+                                </div>
             `
+            
+            
         });
         // const createElement = (tagName, tagContent) =>{
         //     let temp = document.createElement('div');
         //     temp.textContent = str
         //     return temp.innerHTML
         // }
-        // const combineElements = (elements) => {
+        // const combineElements = (elements) => {f
         //     const $fragment = document.createDocumentFragment();
         //     elements.forEach((element) => $fragment.append(element))
         //     return $fragment
-        // }
+        // } 
+        // ${item.soldout === 1 ? <div class ="product-blur"> : ''}
+
+
 
         document.querySelector("#product-list").innerHTML = productLists
     }
