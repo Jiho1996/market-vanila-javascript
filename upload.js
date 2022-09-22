6236e45381b6d6d15d0818177a08fa48e8c2f720
jiho1996 <jiho.urssu@gmail.com>
2022-09-22 16:34:37 +0900

    refactor : upload 컴포넌트 분리

--- upload.js
+++ upload.js
@@ -1,52 +1,12 @@
 import { URL } from "./constants.js"
 import { $ } from "./utils/dom.js"
+import Uploads from "./view/uploads.js"
+
 
 
 export default class uploads{
     constructor(){
-        
         this.render()
-    }
-
-    async getHtml(){
-        return `
-
-    <link href="./upload.css" type="text/css" rel="stylesheet" />
-
-    
-    <div id="upload-container">
-        <img id="preview-image">
-
-        <input id="upload-img-placeholder" type="file" accept ="image/*"/>
-        <input id = "pic-submit-button" type="submit">
-
-        <label for="index_image">이 곳에서 이미지 파일을 선택하세요.</label>
-    </div>
-                <div class="upload-label">
-                <Input id="upload-name" size="large" placeholder="상품 이름"></Input>
-                </div>
-                </form>
-             
-                <form>
-                <div class="upload-label">
-        
-                    <Input id="upload-price" placeholder="상품 가격"></Input>
-                </div>
-                </form>
-          
-                <form>
-                
-                <input id="product-description" placeholder="상품 소개를 적어주세요."></input>
-              
-                </form>
-                
-                    <input id ="submit-button" size="large" type="submit">
-                </input>
-                
-            </form>
-        </div>
-
-        `
     }
 
     onSubmit (){
@@ -79,7 +39,7 @@ export default class uploads{
 
     async render (){
         
-        $('#app').innerHTML = await this.getHtml()
+        new Uploads($('#app'));
         this.initEventListener();
     }
     initEventListener () {
