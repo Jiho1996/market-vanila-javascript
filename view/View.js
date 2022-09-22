85ec0ea40e7130b2a2cd5a48722ad35877a0665e
jiho1996 <jiho.urssu@gmail.com>
2022-09-22 13:39:51 +0900

    refactor : header 컴포넌트 분리

--- view/View.js
+++ view/View.js
@@ -2,15 +2,20 @@ import Core from "./Core/Core.js";
 import Main from "./Home/Main.js";
 import Banner from "./Home/Banner.js";
 import { $ } from "../utils/dom.js";
+import Header from "./Header.js";
 
 export default class View extends Core{
-    async render(){
-        const getProducts = await axios.get('http://127.0.0.1:8080/products');
-        new Banner($("#app"), '');
-        let productList = document.createElement('div');
-        productList.id = 'product-list'
-        $('#app').appendChild(productList)
-        new Main($('#product-list'), getProducts);
+    render(){
+        const makeProductsFrame = () => {
+            let productList = document.createElement('div');
+            productList.id = 'product-list'
+            $('#app').appendChild(productList)
+        };
+        
+        new Header($("#header"));
+        new Banner($("#app"));
+
+        makeProductsFrame();
         
     }
 }
